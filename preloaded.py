4b4541dce7383c221a3dcbe2ed006d5127b5f7f9
raccoon-cai <123484821+raccoon-cai@users.noreply.github.com>
2023-01-29 09:02:05 -0500

    Update preloaded.py

--- preloaded.py
+++ preloaded.py
@@ -6,9 +6,9 @@ from cv2 import imshow, imwrite,waitKey, destroyWindow
 from cv2 import VideoCapture
 
 type_string = input("'roast' or 'compliment'?")
-specific_img = input("'dog', 'cat', or 'cpax'?")
+specific_img = input("'dog.jpeg', 'cat.jpeg', or 'cpax.jpeg'?")
 
 if (type_string == 'roast'):
-    finalRoast("demo/" + specific_img + ".jpeg")
+    finalRoast("demo/" + specific_img)
 elif (type_string == 'compliment'):
-    finalCompliment("demo/" + specific_img + ".jpeg")
+    finalCompliment("demo/" + specific_img)
